68437d3fbfb3f7977b2a5b1b7559b0170b00e7ed
SamikaShrestha <shresthasamika640@gmail.com>
2026-08-20 10:41:27 +0545

    added header and footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <hr>
+    <p>
+        <!-- updates the date itself every year -->
+        &copy Rights <?php echo date("Y") ?> BCA-Ecommerce
+    </p>
+</footer>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+require_once "header.php";
+?>
+
+<h1>Welcome to BCA Ecommerce page</h1>
+
+<?php
+require_once "footer.php";
+?>
